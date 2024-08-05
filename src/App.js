2c3bc10482f578f3e7a9684a46d875fe7c73406d
geethashreep <geethashreepallapothu@Geethashrees-MacBook-Air.local>
2024-08-05 16:31:43 +0530

    pushed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const SearchFilter = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [items, setItems] = useState(['Apple', 'Banana', 'Orange', 'Mango']);
+
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleButtonClick = () => {
+    if (inputValue.trim() !== '') {
+      const itemExists = items.some(item => item.toLowerCase() === inputValue.toLowerCase());
+      if (!itemExists) {
+        setItems([...items, inputValue]);
+      }
+      setInputValue('');
+    }
+  };
+
+
+  const filteredItems = items.filter(item =>
+    item.toLowerCase().includes(inputValue.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>SEARCH FILTER</h1>
+      <input
+        type="text"
+        placeholder='Search'
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleButtonClick}>ADD</button>
+      <ul>
+        {filteredItems.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
-export default App;
+export default SearchFilter;
